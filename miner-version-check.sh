457e4c3ee5e0a4fdd2068e937919a61232c64211
Fabrizio Amodio <ziofabry@hotmail.com>
2022-04-30 21:22:58 +0200

    Fix version check

--- miner-version-check.sh
+++ miner-version-check.sh
@@ -4,5 +4,5 @@ latest=$(curl -s 'https://quay.io/api/v1/repository/team-helium/miner/tag/?limit
             -H 'x-requested-with: XMLHttpRequest' | grep -Po 'miner-arm64_[0-9]+\.[0-9]+\.[0-9]+\.[^"]+_GA' | sort -n | tail -1)
 
 echo -n $latest > /var/dashboard/statuses/latest_miner_version
-docker ps --format "{{.Image}}" --filter "name=helium-miner" | grep -Po "miner-arm64_.*" > /var/dashboard/statuses/current_miner_version
+docker ps --format "{{.Image}}" --filter "name=miner" | grep -Po "miner-arm64_.*" > /var/dashboard/statuses/current_miner_version
 
