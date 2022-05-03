e171f3225599e8a0a56b7cef65e7a24a37c7a535
Fabrizio Amodio <ziofabry@hotmail.com>
2022-05-03 15:49:58 +0200

    Tuning Update #2

--- update.sh
+++ update.sh
@@ -9,14 +9,14 @@ wget https://raw.githubusercontent.com/ZioFabry/p100-scripts/main/miner-version-
 chmod 755 /etc/monitor-scripts/miner-version-check.sh
 
 # tune dashboard timers setting
-sed -i 's/OnBootSec=5/OnBootSec=10/g' bt-check.timer
-sed -i 's/OnUnitActiveSec=5/OnUnitActiveSec=10/g' bt-check.timer
-sed -i 's/OnBootSec=120/OnBootSec=180/g' infoheight-check.timer
-sed -i 's/OnUnitActiveSec=120/OnUnitActiveSec=180/g' infoheight-check.timer
-sed -i 's/OnBootSec=60/OnBootSec=300/g' peer-list-check.timer
-sed -i 's/OnUnitActiveSec=60/OnUnitActiveSec=300/g' peer-list-check.timer
-sed -i 's/OnBootSec=120/OnBootSec=180/g' helium-status-check.timer
-sed -i 's/OnUnitActiveSec=120/OnUnitActiveSec=180/g' helium-status-check.timer
+sed -i 's/OnBootSec=5/OnBootSec=10/g' /etc/systemd/system/bt-check.timer || true
+sed -i 's/OnUnitActiveSec=5/OnUnitActiveSec=10/g' /etc/systemd/system/bt-check.timer || true
+sed -i 's/OnBootSec=120/OnBootSec=180/g' /etc/systemd/system/infoheight-check.timer || true
+sed -i 's/OnUnitActiveSec=120/OnUnitActiveSec=180/g' /etc/systemd/system/infoheight-check.timer || true
+sed -i 's/OnBootSec=60/OnBootSec=300/g' /etc/systemd/system/peer-list-check.timer || true
+sed -i 's/OnUnitActiveSec=60/OnUnitActiveSec=300/g' /etc/systemd/system/peer-list-check.timer || true
+sed -i 's/OnBootSec=120/OnBootSec=180/g' /etc/systemd/system/helium-status-check.timer || true
+sed -i 's/OnUnitActiveSec=120/OnUnitActiveSec=180/g' /etc/systemd/system/helium-status-check.timer || true
 
 # disable recent_activity
 sed -i 's/recent_activity=\$(curl -s \$recent_activity_uri)/recent_activity=disabled/g' /etc/monitor-scripts/helium-statuses.sh
