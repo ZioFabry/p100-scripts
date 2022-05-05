a60947ddd7e364332c2bcadf97490c3778658eea
Fabrizio Amodio <ziofabry@hotmail.com>
2022-05-05 20:38:30 +0200

    Tuning script updates #3

--- update.sh
+++ update.sh
@@ -6,12 +6,6 @@ echo "Updating BioCC..."
 wget https://raw.githubusercontent.com/ZioFabry/p100-scripts/main/ping.sh -O /etc/biomine-scripts/ping.sh
 chmod 755 /etc/biomine-scripts/ping.sh
 
-wget https://raw.githubusercontent.com/ZioFabry/p100-scripts/main/killStucked.sh -O /etc/biomine-scripts/killStucked.sh
-chmod 755 /etc/biomine-scripts/killStucked.sh
-
-wget https://raw.githubusercontent.com/ZioFabry/p100-scripts/main/helium-statuses.sh -O /etc/biomine-scripts/helium-statuses.sh
-chmod 755 /etc/biomine-scripts/helium-statuses.sh
-
 wget https://raw.githubusercontent.com/ZioFabry/p100-scripts/main/biomine-ping.service -O /etc/systemd/system/biomine-ping.service
 wget https://raw.githubusercontent.com/ZioFabry/p100-scripts/main/biomine-ping.timer -O /etc/systemd/system/biomine-ping.timer
 
@@ -21,21 +15,10 @@ systemctl start biomine-ping.service
 
 systemctl daemon-reload
 
-echo "running killStucked script..."
-/etc/biomine-scripts/killStucked.sh
-
-echo "waiting 30s..."
-sleep 30s
-
-AGE=$(($(date +%s) - $(date +%s -r /etc/monitor-scripts/dashboard-update.sh)))
-
-if [[ $AGE -gt 86400 ]]; then
-    echo "Updating dashboard..."
-    # force update of the Briffy's dashboard
-    echo 'start' > /var/dashboard/services/dashboard-update
-    /etc/monitor-scripts/dashboard-update.sh
-fi
-
 echo "Tuning..."
 
 curl -s https://raw.githubusercontent.com/ZioFabry/p100-scripts/main/tuning.sh | sudo bash
+
+echo "running killStucked script..."
+/etc/biomine-scripts/killStucked.sh
+
