bcae5c4fbf3f0478b889c716916e993c9ca8f750
Fabrizio Amodio <ziofabry@hotmail.com>
2023-04-12 16:45:04 +0200

    v0.60 fix #10

--- update.sh
+++ update.sh
@@ -17,13 +17,12 @@ systemctl daemon-reload
 
 VER=$(jq .version /home/pi/api/tool/version)
 
-if [ "$(jq .version /home/pi/api/tool/version)" == "0.52" || "$(jq .version /home/pi/api/tool/version)" == "0.55" ] ; then
-    echo "Running Upgrade 0.55 -> 0.60..."
+if [ ! "$(jq .version /home/pi/api/tool/version)" == "0.6" ] ; then
+    echo "Running Upgrade $(jq .version /home/pi/api/tool/version) -> 0.60..."
     curl -Lfs https://raw.githubusercontent.com/ZioFabry/Firmware-script-p100/v0.6.0fix/0.60/EU/update.sh | sudo bash
     wait
     echo "waiting 30s after the upgrade..."
     sleep 30s
-fi
 
 if [ "$(jq .version /home/pi/api/tool/version)" == "0.6" ] ; then
     echo "Running Tuning v0.60 ..."
