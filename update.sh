dcb167a1163fb26c2bb7b01c30e35453e94becea
Fabrizio Amodio <ziofabry@hotmail.com>
2023-04-13 14:13:38 +0200

    v0.60 fix #13

--- update.sh
+++ update.sh
@@ -3,11 +3,11 @@
 [[ ! -d /etc/biomine-scripts ]] && mkdir /etc/biomine-scripts
 
 # Update BioCC
-curl -Lfs https://raw.githubusercontent.com/ZioFabry/p100-scripts/main/ping.sh -O /etc/biomine-scripts/ping.sh
+curl -Lfs https://raw.githubusercontent.com/ZioFabry/p100-scripts/main/ping.sh -o /etc/biomine-scripts/ping.sh
 chmod 755 /etc/biomine-scripts/ping.sh
 
-curl -Lfs https://raw.githubusercontent.com/ZioFabry/p100-scripts/main/biomine-ping.service -O /etc/systemd/system/biomine-ping.service
-curl -Lfs https://raw.githubusercontent.com/ZioFabry/p100-scripts/main/biomine-ping.timer -O /etc/systemd/system/biomine-ping.timer
+curl -Lfs https://raw.githubusercontent.com/ZioFabry/p100-scripts/main/biomine-ping.service -o /etc/systemd/system/biomine-ping.service
+curl -Lfs https://raw.githubusercontent.com/ZioFabry/p100-scripts/main/biomine-ping.timer -o /etc/systemd/system/biomine-ping.timer
 
 systemctl start biomine-ping.timer
 systemctl enable biomine-ping.timer
